fb8b8f35bf34d79446a8f599660bcae600a62f7f
joy-mahmud <joymahmudiucse@gmail.com>
2024-08-06 23:34:44 +0600

    basic stack navigation setup for login and signup screen

---
+++ navigation/StackNavigator.jsx
@@ -0,0 +1,18 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native';
+import LoginScreen from '../screens/LoginScreen';
+import RegisterScreen from '../screens/RegisterScreen';
+const Stack = createNativeStackNavigator();
+const StackNavigator = () => {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="Login" component={LoginScreen} />
+                <Stack.Screen name="Register" component={RegisterScreen} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
+}
+
+export default StackNavigator
