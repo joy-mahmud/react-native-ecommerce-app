d5bbd011d7e4308166091c6ff108e45a3ce7ac64
joy-mahmud <joymahmudiucse@gmail.com>
2024-08-07 01:01:50 +0600

    login screen partially done

--- navigation/StackNavigator.jsx
+++ navigation/StackNavigator.jsx
@@ -3,12 +3,13 @@ import React from 'react'
 import { NavigationContainer } from '@react-navigation/native';
 import LoginScreen from '../screens/LoginScreen';
 import RegisterScreen from '../screens/RegisterScreen';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 const Stack = createNativeStackNavigator();
 const StackNavigator = () => {
     return (
         <NavigationContainer>
             <Stack.Navigator>
-                <Stack.Screen name="Login" component={LoginScreen} />
+                <Stack.Screen name="Login" component={LoginScreen} options={{headerShown:false}}/>
                 <Stack.Screen name="Register" component={RegisterScreen} />
             </Stack.Navigator>
         </NavigationContainer>
--- screens/LoginScreen.jsx
+++ screens/LoginScreen.jsx
@@ -1,12 +1,17 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { KeyboardAvoidingView, Platform, SafeAreaView, StatusBar, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
 const LoginScreen = () => {
-  return (
-    <View>
-      <Text>LoginScreen</Text>
-    </View>
-  )
+    return (
+        <SafeAreaView style={{ paddingTop: Platform.OS == 'android' ? StatusBar.currentHeight : '' }}>
+            <View style={{flexDirection:'row',justifyContent:'center',paddingVertical:30, }}><Text style={{fontWeight:500,fontSize:20}}>E-com.</Text></View>
+            <KeyboardAvoidingView>
+                <View>
+                    <Text style={{textAlign:'center', fontSize:20}}>Login to your Account</Text>
+                </View>
+            </KeyboardAvoidingView>
+        </SafeAreaView>
+    )
 }
 
 export default LoginScreen
