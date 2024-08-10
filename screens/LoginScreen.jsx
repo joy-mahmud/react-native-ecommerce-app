caa88818b9fab2f716d0684703b11fd6f102c848
joy-mahmud <joymahmudiucse@gmail.com>
2024-08-10 18:07:21 +0600

    login functionality done

--- screens/LoginScreen.jsx
+++ screens/LoginScreen.jsx
@@ -1,13 +1,30 @@
-import { KeyboardAvoidingView, Platform, Pressable, SafeAreaView, ScrollView, StatusBar, StyleSheet, Text, TextInput, View } from 'react-native'
+import { Alert, KeyboardAvoidingView, Platform, Pressable, SafeAreaView, ScrollView, StatusBar, StyleSheet, Text, TextInput, View } from 'react-native'
 import React, { useState } from 'react'
 import Ionicons from '@expo/vector-icons/Ionicons';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { useNavigation } from '@react-navigation/native';
+import axios from 'axios';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const LoginScreen = () => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
-    const navigattion = useNavigation()
+    const navigation = useNavigation()
+    const handleLogin =()=>{
+        const user = {
+            email:email,
+            password:password
+        }
+        axios.post('http://192.168.2.143:8000/login',user)
+        .then((response)=>{
+            console.log(response)
+            const token = response.data.token
+            AsyncStorage.setItem('authToken',token)
+            navigation.replace("Home")
+        }).catch((error)=>{
+            Alert.alert("Login failed","Invalid email")
+        })
+    }
     return (
         <SafeAreaView style={{ paddingTop: Platform.OS == 'android' ? StatusBar.currentHeight : 0, flex: 1 }}>
             <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
@@ -30,13 +47,13 @@ const LoginScreen = () => {
                         <Text style={{ fontSize: 16, fontWeight: 500, color: '#007fff' }}>Forgot password?</Text>
                     </View>
                     <View style={{ marginTop: 70, alignItems: 'center' }}>
-                        <Pressable style={{ width: 200, backgroundColor: '#febe10', borderRadius: 6, paddingVertical: 10 }}>
+                        <Pressable onPress={handleLogin} style={{ width: 200, backgroundColor: '#febe10', borderRadius: 6, paddingVertical: 10 }}>
                             <Text style={{ textAlign: 'center', color: 'white', fontSize: 16, fontWeight: 'bold', }}>Login</Text>
 
                         </Pressable>
                         <Pressable style={{ marginTop: 15, flexDirection: 'row', gap: 5 }}>
                             <Text style={{ fontSize: 16 }}>Dont have an account?</Text>
-                            <Text onPress={() => navigattion.navigate('Register')} style={{ textAlign: 'center', color: '#007ffe', fontSize: 16, fontWeight: 'bold', }}>Sign up</Text>
+                            <Text onPress={() => navigation.navigate('Register')} style={{ textAlign: 'center', color: '#007ffe', fontSize: 16, fontWeight: 'bold', }}>Sign up</Text>
 
                         </Pressable>
                     </View>
